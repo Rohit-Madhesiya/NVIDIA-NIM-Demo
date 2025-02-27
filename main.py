6e9ed8bfbb1e25b165a3c249239a6a37cb2a5073
Rohit Gupta <rohitmadhesiya19@gmail.com>
2025-02-27 16:09:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ if st.button("Document Embedding"):
   vector_embedding()
   st.write("FAISS Vector Store Db is Ready using NVIDIAEmbedding")
 
-if question:
+if st.button("Ask Question"):
   document_chain=create_stuff_documents_chain(llm,prompt)
   retriever=st.session_state.vectors.as_retriever()
   retrieval_chain=create_retrieval_chain(retriever,document_chain)
